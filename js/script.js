c425fb3e7693c8f862768b0d7f69f05403e2a0bc
willshown <w@willshown.com>
2012-08-07 17:43:20 -0400

    This script should work…

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@
       var canvas = $('canvas.faces').get(0);
 
       if (typeof window.html5media !== "undefined") {
-        canvas.onload = semaphore(canvas, v, $);
         v = window.flowplayer(0);
+        canvas.onload = semaphore(v);
         v.onLoad(function(){
-          semaphore(canvas, v, $);
+          semaphore(v);
         });
       }else{
         startNative(canvas, v, $);
@@ -71,35 +71,12 @@
 
   var sem = 0;
 
-  function semaphore(canvas, v, $) {
-    if (sem == 1){startShim(canvas, v, $)}else{sem += 1}
+  function semaphore(v) {
+    if (sem == 1){startShim(v)}else{sem += 1}
   }
 
-  function drawShim(v, f, c){
-    //if(v.getState() !== 3 ){ console.log('Nothing to draw.'); return false; }
-    var i = new Image();
-    i.src = "data:image/png;base64," + f.getScreenshot();
-    c.drawImage(i, 0, 0, i.width, i.height);
-    //setTimeout(drawShim(v,f,c),20);
-  }
-
-  function startShim(canvas, v, $){
-    var context = canvas.getContext('2d');
-    var c = v.getClip(0);
-    var f = $('#facesVideo_api').get(0);
-
-    c.onStart(function(){
-      canvas.width = c.metaData.width;
-      canvas.height = c.metaData.height;
-    });
-
-    c.onResume(function(){
-      console.log('Drawing on resume.');
-      //drawShim(v, f, context);
-    });
-
-    v.startBuffering();
-    setTimeout(function(){v.play();}, 2000);
+  function startShim(v){
+    v.play();
   }
 
 })(window);
